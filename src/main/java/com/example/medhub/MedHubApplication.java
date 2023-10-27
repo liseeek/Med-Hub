ef905aec529c77e1df0aed311e47a6075b1fc3d7
liseeek <lisszymon.contact@gmail.com>
2023-10-27 17:35:30 +0200

    Initialization

---
+++ src/main/java/com/example/medhub/MedHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.medhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedHubApplication.class, args);
+    }
+
+}
